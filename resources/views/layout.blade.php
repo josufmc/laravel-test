1bf956ab4d142c22ca2a31ed267baeae4a2e41b0
Josu <josufmc@hotmail.com>
2017-10-28 13:14:27 +0200

    Laravel: Added some styles

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="{{ url('/') }}/css/app.css">
         <title>Laravel</title>
     </head>
     <body>
@@ -13,18 +14,38 @@
                 return request()->is($url) ? 'active' : '';
             }
             ?>
-            <nav>
-                <a class="{{ activeMenu('/') }}" href="{{ route('home') }}"> Home </a>
-                <a class="{{ activeMenu('saludos/*') }}" href="{{ route('saludo', 'Josu') }}"> Saludos </a>
-                <a class="{{ activeMenu('messages/create') }}" href="{{ route('messages.create') }}"> Crear mensaje </a>
-                @if (auth()->check())
-                    <a class="{{ activeMenu('messages') }}" href="{{ route('messages.index') }}"> Mensajes </a>
-                    <a class="{{ activeMenu('logout') }}" href="{{ route('logout') }}"> Cerrar sesión de {{ auth()->user()->name }} </a>
-                @endif
-                @if (auth()->guest())
-                    <a class="{{ activeMenu('login') }}" href="{{ route('login') }}"> Login </a>
-                @endif
-            </nav>
+            <div class="collapse navbar-collapse" id="collapsibleNavId">
+                <ul class="nav navbar-nav">
+                    <li class="nav-item {{ activeMenu('/') }}">
+                        <a class="" href="{{ route('home') }}"> Home </a>
+                    </li>
+                    <li class="nav-item {{ activeMenu('saludos/*') }}">
+                        <a class="" href="{{ route('saludo', 'Josu') }}"> Saludos </a>
+                    </li>
+                    <li class="nav-item {{ activeMenu('messages/create') }}">
+                        <a class="" href="{{ route('messages.create') }}"> Crear mensaje </a>
+                    </li>
+
+                    @if (auth()->check())
+                        <li class="nav-item {{ activeMenu('messages') }}">
+                            <a class="" href="{{ route('messages.index') }}"> Mensajes </a>
+                        </li>
+                    @endif
+                </ul>
+
+                <ul class="nav navbar-nav navbar-right">
+                    @if (auth()->check())
+                        <li class="nav-item {{ activeMenu('logout') }}">
+                            <a class="" href="{{ route('logout') }}"> Cerrar sesión de {{ auth()->user()->name }} </a>
+                        </li>
+                    @endif
+                    @if (auth()->guest())
+                        <li class="nav-item {{ activeMenu('login') }}">
+                            <a class="" href="{{ route('login') }}"> Login </a>
+                        </li>
+                    @endif
+                </ul>
+            </div>
         </header>
         <div class="content">
             @yield('contenido')
