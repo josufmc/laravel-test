406daa2b6f2cd4d160436c85f12dd8691d68f46f
josufmc@hotmail.com <josufmc@hotmail.com>
2017-11-28 19:15:56 +0100

    Added some unit test for controller

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -3,20 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Message;
+use Illuminate\Routing\Redirector;
 use Illuminate\Support\Facades\Cache;
 use App\Events\MessageWasRecievedEvent;
 use App\Repositories\IMessagesRepository;
+use Illuminate\Events\Dispatcher as Event;
+use Illuminate\View\Factory as ViewFactory;
 use Symfony\Component\HttpFoundation\Request;
 use App\Repositories\CacheMessagesRepository;
 
 class MessagesController extends Controller
 {
     
-    private $messagesRepository;
+    protected $messagesRepository;
+    protected $view;
+    protected $redirect;
 
-    public function __construct(IMessagesRepository $messagesRepository){
-        $this->middleware('auth', ['except' => ['create', 'store']]);
+    public function __construct(IMessagesRepository $messagesRepository, ViewFactory $view, Redirector $redirect){
         $this->messagesRepository = $messagesRepository;
+        $this->view = $view;
+        $this->redirect = $redirect;
+        $this->middleware('auth', ['except' => ['create', 'store']]);
     }
     
     /**
@@ -27,7 +34,7 @@ class MessagesController extends Controller
     public function index()
     {
         $messages = $this->messagesRepository->getPaginatedMessages();
-        //return view("messages.index", compact('messages'));
+        return $this->view->make("messages.index", compact('messages'));
     }
 
     /**
@@ -37,7 +44,7 @@ class MessagesController extends Controller
      */
     public function create()
     {
-        return view('messages.create');
+        return $this->view->make('messages.create');
     }
 
     /**
@@ -47,11 +54,11 @@ class MessagesController extends Controller
      * @return \Illuminate\
      * Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, Event $event)
     {
         $message = $this->messagesRepository->storeMessage($request);
-        event(new MessageWasRecievedEvent($message));
-        return redirect()->route('messages.create')->with('info', 'Mensaje creado!');
+        $event->fire(new MessageWasRecievedEvent($message));
+        return $this->redirect->route('messages.create')->with('info', 'Mensaje creado!');
     }
 
     /**
@@ -63,7 +70,7 @@ class MessagesController extends Controller
     public function show($id)
     {
         $message = $this->messagesRepository->findById($id);
-        return view("messages.show", compact('message'));
+        return $this->view->make("messages.show", compact('message'));
     }
 
     /**
@@ -75,7 +82,7 @@ class MessagesController extends Controller
     public function edit($id)
     {
         $message = $this->messagesRepository->findById($id);
-        return view('messages.edit', compact('message'));
+        return $this->view->make('messages.edit', compact('message'));
     }
 
     /**
@@ -88,7 +95,7 @@ class MessagesController extends Controller
     public function update(Request $request, $id)
     {
         $this->messagesRepository->update($request, $id);
-        return redirect()->route('messages.index');
+        return $this->redirect->route('messages.index');
     }
 
     /**
@@ -100,6 +107,6 @@ class MessagesController extends Controller
     public function destroy($id)
     {
         $this->messagesRepository->destroy($id);
-        return redirect()->route('messages.index');
+        return $this->redirect->route('messages.index');
     }
 }
--- tests/Feature/unit/MessagesControllerTest.php
+++ tests/Feature/unit/MessagesControllerTest.php
@@ -9,20 +9,146 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class MessagesControllerTest extends TestCase
 {
-    public function testIndex()
-    {
-        $messagesRepository = \Mockery::mock('App\Repositories\MessagesRepository');
-        $controller = new MessagesController($messagesRepository);
+    protected $messagesRepository;
+    protected $view;
+    protected $controller;
+    protected $redirect;
+    protected $request;
+
+    public function setUp(){
+        $this->messagesRepository = \Mockery::mock('App\Repositories\MessagesRepository');
+        $this->view = \Mockery::mock('Illuminate\View\Factory');
+        $this->redirect = \Mockery::mock('Illuminate\Routing\Redirector');
+        $this->request = \Mockery::mock('Illuminate\Http\Request');
         
-        // Verificar que se llame al método getPaginated sólo una vez
-        $messagesRepository->shouldReceive('getPaginated')->once()->withAnyArgs();
-
-        // Llamamos al metodo index
-        $controller->index();
-
+        $this->controller = new MessagesController($this->messagesRepository, $this->view, $this->redirect);
     }
-
+    
     public function tearDown(){
         \Mockery::close();
     }
+
+    public function testIndex()
+    {
+        // Verificar que se llame al método getPaginatedMessages sólo una vez
+        // Creamos como resultado una cadena 
+        $this->messagesRepository->shouldReceive('getPaginatedMessages')->once()->andReturn('paginated_messages');
+        // Verificamos que se llama al método make de la View factory
+        $this->view
+            ->shouldReceive('make')
+            ->with('messages.index', ['messages' => 'paginated_messages'])
+            ->once();
+        // Llamamos al metodo index
+        $this->controller->index();
+    }
+
+    public function testCreate(){
+        $this->view
+            ->shouldReceive('make')
+            ->with('messages.create')
+            ->once();
+
+        // Original method
+        $this->controller->create();
+    }
+
+    public function testStore(){
+        $event = \Mockery::mock('Illuminate\Events\Dispatcher');
+        $messageEvent = \Mockery::type('App\Events\MessageWasRecievedEvent');   // Instanceof Un tipo de (Para comprobar tipos)
+
+        $this->messagesRepository
+            ->shouldReceive('storeMessage')
+            ->with($this->request)
+            ->once()
+            ->andReturn('saved_message')
+            ;
+        
+        
+        //$event->shouldReceive('fire')->once();
+        $event->shouldReceive('fire')
+            ->once()
+            ->with(\Mockery::on(function($param){
+                //dd($param);
+                //return $param instanceof App\Events\MessageWasRecievedEvent && $param->message == 'saved_message';
+                return $param->message == 'saved_message';
+            }));
+
+        $this->redirect
+            ->shouldReceive('route')
+            ->once()
+            ->with('messages.create')
+            ->andReturn($this->redirect)   // Se devuelve a sí mismo para usar el with
+            ;
+
+        $this->redirect
+            ->shouldReceive('with')
+            ->once()
+            ->with('info', 'Mensaje creado!');
+
+        // Original method
+        $this->controller->store($this->request, $event);
+    }
+
+    public function testShow(){
+        $id = 1;
+        
+        $this->messagesRepository->shouldReceive('findById')->with($id)->once()->andReturn('found_message');
+
+        $this->view
+            ->shouldReceive('make')
+            ->with('messages.show', ['message' => 'found_message'])
+            ->once()
+            ;
+
+        // Original method
+        $this->controller->show($id);
+    }
+
+
+    public function testEdit(){
+        $id = 1;
+        
+        $this->messagesRepository->shouldReceive('findById')->with($id)->once()->andReturn('found_message');
+
+        $this->view
+            ->shouldReceive('make')
+            ->with('messages.edit', ['message' => 'found_message'])
+            ->once()
+            ;
+
+        // Original method
+        $this->controller->edit($id);
+    }
+
+    public function testUpdate(){
+        $id = 1;
+        
+        $this->messagesRepository->shouldReceive('update')->with($this->request, $id)->once();
+
+        $this->redirect
+            ->shouldReceive('route')
+            ->once()
+            ->with('messages.index')
+            ;
+
+        // Original method
+        $this->controller->update($this->request, $id);
+    }
+
+    public function testDestroy(){
+        $id = 1;
+
+        $this->messagesRepository->shouldReceive('destroy')->with($id)->once();
+
+        $this->redirect
+            ->shouldReceive('route')
+            ->once()
+            ->with('messages.index')
+            ;
+
+        // Original method
+        $this->controller->destroy($id);
+    }
+
+    
 }
